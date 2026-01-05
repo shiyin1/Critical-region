19e32a15950b1a25d727d9bdd75ba7ecb55ab191
yinshi2018 <shiyin_dalian@163.com>
2026-01-05 22:54:01 +0100

    data

---
+++ critical_region/LPAp/beta/LOdata/plot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# sphinx_gallery_thumbnail_number = 3
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import NullFormatter  # useful for `logit` scale
+import matplotlib.ticker as ticker
+import matplotlib as mpl
+
+mpl.style.use('classic')
+
+# Data for plotting
+#T=np.loadtxt('Tem1/buffer/TMeV.dat')
+mub0data=np.loadtxt('./mub0.dat')
+mub300data=np.loadtxt('./mub300.dat')
+mub400data=np.loadtxt('./mub400.dat')
+mub500data=np.loadtxt('./mub500.dat')
+mub600data=np.loadtxt('./mub600.dat')
+beta=0.395
+# Create figure
+fig=plt.figure(figsize=(4.5, 3.5))
+#fig=plt.figure()
+ax1=fig.add_subplot(111)
+ax1.plot(np.exp(mub0data[:,0]),mub0data[:,1],'-',c='#F4DDB6',linewidth=2.5,alpha=1.,label=r'$\mu_B=0$')
+ax1.plot(np.exp(mub300data[:,0]),mub300data[:,1],'-',c='#F5CABC',linewidth=2.5,alpha=1.,label=r'$\mu_B=300\,\mathrm{MeV}$')
+ax1.plot(np.exp(mub400data[:,0]),mub400data[:,1],'-',c='#F4B4BE',linewidth=2.5,alpha=1.,label=r'$\mu_B=400\,\mathrm{MeV}$')
+ax1.plot(np.exp(mub500data[:,0]),mub500data[:,1],'-',c='#CEA2B5',linewidth=2.5,alpha=1.,label=r'$\mu_B=500\,\mathrm{MeV}$')
+ax1.plot(np.exp(mub600data[:,0]),mub600data[:,1],'-',c='#9C9CC0',linewidth=2.5,alpha=1.,label=r'$\mu_B=600\,\mathrm{MeV}$')
+ax1.plot([10**-12,10**4],[beta,beta],dashes=[2,1],color='gray',linewidth=1.,alpha=0.6)
+ax1.fill_between([10**-12,10**4],np.array([beta,beta])*0.99,np.array([beta,beta])*1.01,color='gray',edgecolor='none',alpha=0.3)
+ax1.fill_betweenx([0,1],[10**-6,10**-6],[0.01/700,0.01/700],color='red',edgecolor='none',alpha=0.3,zorder=10)
+#ax1.text(221,1.2,r'$\mu=290\,\mathrm{MeV}$',fontsize=10)
+ax1.set_xscale('log')
+ax1.axis([6*10**-6,0.03,0.3,0.45])
+
+ax1.set_xlabel(r'$t$', fontsize=14, color='black')
+ax1.set_ylabel(r'$\mathrm{Leading\,\,order\,\,Fit}\,\,\beta$', fontsize=14, color='black')
+
+ax1.legend(loc=0,fontsize=9.5,frameon=False,shadow=True,handlelength=3.,borderpad=0.5,borderaxespad=1,scatterpoints=1)
+
+for label in ax1.xaxis.get_ticklabels():
+    label.set_fontsize(10)
+for label in ax1.yaxis.get_ticklabels():
+    label.set_fontsize(10)
+
+fig.subplots_adjust(top=0.9, bottom=0.14, left=0.145, right=0.95, hspace=0.35,wspace=0.35)
+
+fig.savefig("beta_LO.pdf")
