6cb2e8b1f1a6dcdf5b464ead84aeb0d3108c2bbe
shi <shi.yin@theo.physik.uni-giessen.de>
2026-01-11 20:48:01 +0100

    plot

--- critical_region/3D_phase_plot/plot.py
+++ critical_region/3D_phase_plot/plot.py
@@ -7,10 +7,12 @@ import numpy as np
 from matplotlib.ticker import NullFormatter  # useful for `logit` scale
 import matplotlib.ticker as ticker
 import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D 
 
 mpl.style.use('classic')
 
 # Data for plotting
+pb=np.loadtxt('./pb.dat')
 x = np.linspace(-2, 2, 100)
 y = np.linspace(-2, 2, 100)
 X, Y = np.meshgrid(x, y)
@@ -19,21 +21,37 @@ Z = X**2 + Y**2
 fig=plt.figure(figsize=(4.5, 3.5))
 #fig=plt.figure()
 ax1=fig.add_subplot(111, projection='3d')
-ax1.plot_surface(X, Y, Z)
-#ax1.text(221,1.2,r'$\mu=290\,\mathrm{MeV}$',fontsize=10)
-#ax1.set_xscale('log')
-#ax1.axis([6*10**-6,0.03,0.3,0.45])
+ax1.plot(pb[:,0],pb[:,1]*0,pb[:,1],'-',c='#F4DDB6',linewidth=2.5,alpha=1.,label=r'$\mu_B=0$')
+ax1.quiver(0, 0, pb[0,1], 0, 30, 0, arrow_length_ratio=0.2, color='b', linewidth=1.)
 
-ax1.set_xlabel(r'$t$', fontsize=14, color='black')
-ax1.set_ylabel(r'$\mathrm{Leading\,\,order\,\,Fit}\,\,\beta$', fontsize=14, color='black')
+# 黑色主坐标轴（真实物理轴）
+ax1.quiver(0, 0, 0, 700, 0, 0, arrow_length_ratio=0.05, color='k', linewidth=1.5)
+ax1.quiver(0, 0, 0, 0, 50, 0, arrow_length_ratio=0.1, color='k', linewidth=1.5)
+ax1.quiver(0, 0, 0, 0, 0, 150, arrow_length_ratio=0.1, color='k', linewidth=1.5)
 
-ax1.legend(loc=0,fontsize=9.5,frameon=False,shadow=True,handlelength=3.,borderpad=0.5,borderaxespad=1,scatterpoints=1)
+ax1.set_xlim(0, 700)
+ax1.set_ylim(0, 50)
+ax1.set_zlim(0, 150)
 
-for label in ax1.xaxis.get_ticklabels():
-    label.set_fontsize(10)
-for label in ax1.yaxis.get_ticklabels():
-    label.set_fontsize(10)
+ax1.view_init(elev=20, azim=-45)
 
-fig.subplots_adjust(top=0.9, bottom=0.14, left=0.145, right=0.95, hspace=0.35,wspace=0.35)
+# 背景轴设置
+ax1.xaxis.pane.set_visible(False)
+ax1.yaxis.pane.set_visible(False)
+ax1.zaxis.pane.set_visible(False)
+ax1.invert_yaxis()
+ax1.set_xlabel(r'$\mu$', fontsize=14, color='black')
+ax1.set_ylabel(r'$m_\pi$', fontsize=14, color='black')
+ax1.set_zlabel(r'$T$', fontsize=14, color='black')
 
-fig.savefig("beta_LO.pdf")
+for axis in [ax1.xaxis, ax1.yaxis, ax1.zaxis]:
+    axis.line.set_color((0.7, 0.7, 0.7))
+    axis.line.set_linewidth(0.8)
+
+ax1.tick_params(colors='0', width=0.8, labelsize=8)
+
+#fig.subplots_adjust(top=0.9, bottom=0.14, left=0.1, right=0.95, hspace=0.35,wspace=0.35)
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+ax1.set_position([0.0, 0.05, 0.95, 0.95])
+
+fig.savefig("phase_3D.pdf")
