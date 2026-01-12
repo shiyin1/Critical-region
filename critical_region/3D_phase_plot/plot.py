c32594180111088e24bdbba4d0625fb9e0a20fd7
shi <shi.yin@theo.physik.uni-giessen.de>
2026-01-12 17:01:04 +0100

    plot

--- critical_region/3D_phase_plot/plot.py
+++ critical_region/3D_phase_plot/plot.py
@@ -16,30 +16,31 @@ pb=np.loadtxt('./pb.dat')
 mub=np.loadtxt('./mub.dat')
 beta=np.loadtxt('./betaregion.dat')
 
-x = np.linspace(-2, 2, 100)
-y = np.linspace(-2, 2, 100)
-X, Y = np.meshgrid(x, y)
-Z = X**2 + Y**2
+pb2=np.loadtxt('./pb2.dat')
+mub2=np.loadtxt('./mub2.dat')
+delta=np.loadtxt('./deltaregion.dat')
+
 # Create figure
 fig=plt.figure(figsize=(4.5, 3.5))
 #fig=plt.figure()
 ax1=fig.add_subplot(111, projection='3d')
-ax1.plot(pb[:,0],pb[:,1]*0,pb[:,1],'-',c='b',linewidth=1.,alpha=1.,label=r'$\mu_B=0$')
-#ax1.plot(mub,mub*0,beta,'-',c='#F4DDB6',linewidth=2.5,alpha=1.,label=r'$\mu_B=0$')
+l=30
+ax1.plot(pb[:,0],pb[:,1]*0-3*l,pb[:,1],'-',c='b',linewidth=1.,alpha=1.,label=r'$\mu_B=0$')
 
-ax1.fill_between(mub, mub*0, beta ,mub,mub*0, pb[:,1], color='skyblue', alpha=0.5, edgecolor='none',zorder=0)  # 仍然是 2D
-ax1.quiver(0, 0, pb[0,1], 0, 30, 0, arrow_length_ratio=0.25, color='b', linewidth=1.)
-
-# 黑色主坐标轴（真实物理轴）
-ax1.quiver(0, 0, 0, 700, 0, 0, arrow_length_ratio=0.05, color='k', linewidth=1.5)
-ax1.quiver(0, 0, 0, 0, 50, 0, arrow_length_ratio=0.15, color='k', linewidth=1.5)
-ax1.quiver(0, 0, 0, 0, 0, 160, arrow_length_ratio=0.15, color='k', linewidth=1.5)
+ax1.fill_between(mub, mub*0-3*l, beta ,mub,mub*0-3*l, pb[:,1], color='skyblue', alpha=0.5, edgecolor='none',zorder=0)  
+ax1.fill_between(mub2,delta*0-3*l, pb2[:,1],mub2, np.log(delta)*l, pb2[:,1] , color='#F4DDB6', alpha=0.5, edgecolor='none',zorder=0)  
+ax1.quiver(0, 0-3*l, pb[0,1], 0, 6*l, 0, arrow_length_ratio=0.1, color='b', linewidth=1.)
+#黑色主坐标轴（真实物理轴）
+ax1.quiver(0, -3*l, 0, 700, 0, 0, arrow_length_ratio=0.05, color='k', linewidth=1.5)
+ax1.quiver(0, -3*l, 0, 0, 8*l, 0, arrow_length_ratio=0.1, color='k', linewidth=1.5)
+ax1.quiver(0, -3*l, 0, 0, 0, 160, arrow_length_ratio=0.15, color='k', linewidth=1.5)
 
 ax1.set_xlim(0, 700)
-ax1.set_ylim(0, 50)
+ax1.set_ylim(-3*l, 5*l)
 ax1.set_zlim(0, 160)
-
+#ax1.set_box_aspect((1, 1, 1))
 ax1.view_init(elev=20, azim=-45)
+#ax1.view_init(elev=20, azim=-160)
 
 # 背景轴设置
 ax1.xaxis.pane.set_visible(False)
@@ -49,7 +50,9 @@ ax1.invert_yaxis()
 ax1.set_xlabel(r'$\mu$', fontsize=14, color='black')
 ax1.set_ylabel(r'$m_\pi$', fontsize=14, color='black')
 ax1.set_zlabel(r'$T$', fontsize=14, color='black')
-
+# ax1.set_yscale(    'symlog',
+#     linthresh=1e-3,   # 线性区间半宽
+#     linscale=1.0)
 for axis in [ax1.xaxis, ax1.yaxis, ax1.zaxis]:
     axis.line.set_color((0.7, 0.7, 0.7))
     axis.line.set_linewidth(0.8)
